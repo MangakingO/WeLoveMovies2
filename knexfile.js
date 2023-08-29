ff2be4418fd648fe9fcf48a93ea1e96e9775f172
Oscar Barrios Jimenez <102126868+MangakingO@users.noreply.github.com>
2023-08-29 18:13:13 -0500

    first commit

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const path = require("path");
+
+// if (process.env.USER) require("dotenv").config();
+
+const {
+  DATABASE_URL = "postgres://wguitzrc:baCtp4iBL9kisthTYnxlTq1-QcoPSAN7@berry.db.elephantsql.com/wguitzrc",
+} = process.env;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
